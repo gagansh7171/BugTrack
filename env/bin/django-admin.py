a203d2ef919676017936af2f62af17e95f2c32de
Gagan Sharma <gagan@gagan-HP-Pavilion-Gaming-Laptop-15-dk0xxx>
2020-07-29 21:50:27 +0530

    Initialized repository

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/gagan/Documents/Repo/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ project/bug/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
+
+class Users
